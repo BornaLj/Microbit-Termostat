9ea798708558c490aefca0b3cc28bf310a9b9fe7
BornaLj <106666577+BornaLj@users.noreply.github.com>
2022-11-05 23:07:30 +0100

    Update baza.py

--- baza.py
+++ baza.py
@@ -3,12 +3,6 @@ from microbit import *
 import radio
 #import log    # debug/comment
 
-# set local variables for time as hour, minute, seconds
-# change variables for case
-hour = 22
-minute = 29
-second = 50
-
 # set radio group and turn radio on
 radio.config(group=1)
 radio.on()
@@ -16,33 +10,15 @@ radio.on()
 # show SQUARE if micro:bit is ready
 display.show(Image.SQUARE)    # debug/comment
 
-# test data logging
-# log.set_labels('time', 'id', 'temp')    # debug/comment
-
 # loop repeats forever
 while True:
-    
-    # set hours by pressing button A
-    if button_a.is_pressed():
-        hour += 1
-        if hour > 23:
-            hour = 0
-        display.scroll(hour)
-        display.show(":")
-        sleep(100)   # debug/comment
-    
-    # set minutes by pressing button B
-    if button_b.is_pressed():
-        minute += 1
-        if minute > 59:
-            minute = 0
-        display.scroll(minute)
-        display.show(":")
-        sleep(100)   # debug/comment
 
-    # switch into send/receive mode by shaking micro:bit
-    if accelerometer.was_gesture('shake'):
+    # switch into send/receive mode by pressing button A
+    if button_a.is_pressed():
         while True:
+            if button_b.was_pressed():
+                data = data + "Gathered data"
+                uart.write(data)
             display.show(Image.CONFUSED)   # display CONFUSED if in send/receive
             sleep(1000)
             # increase seconds, minutes and hours
@@ -54,7 +30,27 @@ while True:
                 hour += 1
                 minute = 0
             if hour >= 24:
+                day += 1
                 hour = 0
+            
+            #Changing the month and the year
+            if month == 2:
+                if year%4 == 0 and day == 29:
+                    month += 1
+                    day = 1
+                elif day == 28:
+                    month += 1
+                    day = 1
+                elif month in [1, 3, 5, 7, 8, 10, 12] and day == 31:
+                    if month == 12:
+                        year += 1
+                        month = 1
+                        day = 1
+                    else:
+                        month += 1
+                elif month in [2, 4, 6, 9, 11] and day == 30:
+                    month += 1
+
             
             # send request to sub units
             print(str(hour),":",str(minute),":",str(second))   # debug/comment
@@ -68,7 +64,8 @@ while True:
             # when response is received
             response = radio.receive()
             if response:
-                time = str(hour) + ":" + str(minute) + ":" + str(second)
+                time = str(hour) + ":" + str(minute)
+                date = (f"{day}.{month}.{year}")
                 id = str(response).split(":")[0]
                 temp = str(response).split(":")[1]
 
@@ -76,6 +73,3 @@ while True:
                 display.show(id)   # debug/comment
                 display.scroll(response)   # debug/comment
                 display.clear()   # debug/comment
-                
-                # log responses received
-                # log.add({'time': time, 'id' : id, 'temp': temp})
