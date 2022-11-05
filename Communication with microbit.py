68148aa6b426d940ccb36725cad6c5a88cb676f6
BornaLj <106666577+BornaLj@users.noreply.github.com>
2022-11-05 17:48:42 +0100

    Update Communication with microbit.py

--- Communication with microbit.py
+++ Communication with microbit.py
@@ -1,4 +1,5 @@
 import serial
+import serial.tools.list_ports
 import datetime
 import time
 
@@ -9,7 +10,17 @@ while True:
     if ulaz == "START":
 
         #sending date and time
-        ser = serial.Serial("COM3", 9600, timeout=.1)
+        ports = list(serial.tools.list_ports.comports())
+        for p in ports:
+            try:
+                ser = serial.Serial(p, 9600, timeout=.1)
+                break
+            except:
+                if ports.index(p) == len(ports)-1:
+                    print("Mikrobit nije prikopčan na aktivan port. Ponovo pokrenite program i pokušajte ponovo")
+                    quit()
+                else:
+                    continue
         time.sleep(1)
         slanje = (f"{datetime.datetime.now().hour} {datetime.datetime.now().minute} {datetime.datetime.now().second} {datetime.datetime.now().day} {datetime.datetime.now().month} {datetime.datetime.now().year}")
         ser.write(slanje.encode())
@@ -22,6 +33,8 @@ while True:
                 #writing out gathered data from microbit
                 if data[-1] == "Gathered data":
                     data.pop(-1)
+            else:
+                continue
     else:
         while ulaz != "START":
             ulaz = input("Pogreška u unosu, napišite START kako biste započeli")
