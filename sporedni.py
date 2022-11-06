12e4032fac9897ecbc12ef7bffa88c646f97bbf0
BornaLj <106666577+BornaLj@users.noreply.github.com>
2022-11-06 12:59:35 +0100

    Add files via upload

---
+++ sporedni.py
@@ -0,0 +1,23 @@
+from microbit import *
+import radio
+
+#User setts the microbit id
+id = 1
+while True:
+    if button_a.was_pressed():
+        id += 1
+    elif button_b.was_pressed():
+        break
+    
+radio.config(group=1)
+radio.on()
+
+while True:
+    message = radio.receive()
+    display.scroll(str(id))   # debug/comment
+    if message == "+":
+        display.show(message)   # debug/comment
+        sleep(id*100)   # do not remove this line
+        response = (str(id) + ":" + str(temperature()))
+        radio.send(response)
+        display.scroll(str(temperature()))   # debug/comment
